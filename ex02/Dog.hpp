6ac1506e37890293a9632777696103ffe84cef46
mlaporte <mlaporte@student.42.fr>
2024-12-13 17:47:00 +0100

    fixed

---
+++ ex02/Dog.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaporte <mlaporte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 11:49:25 by mlaporte          #+#    #+#             */
+/*   Updated: 2024/11/14 11:49:25 by mlaporte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include <iostream>
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+
+class Dog : public AAnimal
+{
+public:
+	Dog();
+	Dog(const Dog &copy);
+	~Dog();
+
+	Dog &operator=(const Dog &copy);
+
+	void makeSound() const;
+	std::string getType() const;
+	Brain *getBrain() const;
+
+private:
+	std::string _type;
+	Brain *_brain;
+
+};
+#endif
